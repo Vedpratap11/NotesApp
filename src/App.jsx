5f1fa079a835b298321297cc01e62486d36331cd
Vedpratap11 <rajawat.ved1117@gmail.com>
2025-02-26 15:45:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+import { TiDeleteOutline } from "react-icons/ti";
+import { useState } from "react";
+function App() {
+  const [allMessage, setAllMessage] = useState([]);
+  const [text, setText] = useState();
+  const [color, setColor] = useState();
+  const [undoNote, setUndoNote] = useState([]);
+
+  function handleNote(e) {
+    e.preventDefault();
+    const obj = {
+      id: Date.now(),
+      Message: text,
+      Color: color,
+      createdAt: new Date().toLocaleString(),
+    };
+    setAllMessage([...allMessage, obj]);
+    setText("");
+    setColor("");
+  }
+
+  function handleDeleteNote(id) {
+    setAllMessage(allMessage.filter((item) => item.id !== id));
+  }
+  function undoMessage() {
+    const note = allMessage;
+    const lastCreatedNote = note.pop();
+    setUndoNote((prev) => [...prev, lastCreatedNote]);
+  }
+
+  return (
+    <div className="mainDiv">
+      <div className="noteDiv1">
+        <form onSubmit={handleNote}>
+          <textarea
+            name="task"
+            className="taskArea"
+            placeholder="Enter your message"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            required
+          ></textarea>
+          <input
+            type="color"
+            className="colorPalette"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+          />
+          <button className="btn" type="submit">
+            Add Note
+          </button>
+          <button className="btn1" onClick={undoMessage}>
+            Undo
+          </button>
+        </form>
+      </div>
+      <div className="noteDiv2">
+        {allMessage.map((item) => {
+          return (
+            <div
+              key={item.id}
+              style={{ backgroundColor: item.Color }}
+              className="note"
+            >
+              <span className="cross" onClick={() => handleDeleteNote(item.id)}>
+                <TiDeleteOutline />
+              </span>
+              <div className="message">{item.Message}</div>
+              
+              <div className="afterd">{item.createdAt}</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
